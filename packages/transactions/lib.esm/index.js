12ca30ca56f6c4857d5003a2f4a4a92421eefd3e
Ivo Yankov <ivo@devlabs.bg>
2022-02-17 16:39:36 +0200

    fix: nanoseconds part of `transactionId` is sometimes only 8 symbols long instead of 9
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- packages/transactions/lib.esm/index.js
+++ packages/transactions/lib.esm/index.js
@@ -242,8 +242,10 @@ export function parse(rawTransaction) {
             logger.throwArgumentError(error.message, "rawTransaction", rawTransaction);
         }
         const tx = parsed.transactionId;
+        const nanos = tx.validStart.nanos.toString().padStart(9, '0');
+        const seconds = tx.validStart.seconds.toString().padStart(10, '0');
         let contents = {
-            transactionId: tx.accountId.toString() + '-' + tx.validStart.seconds + '-' + tx.validStart.nanos,
+            transactionId: `${tx.accountId.toString()}-${seconds}-${nanos}`,
             hash: hexlify(yield parsed.getTransactionHash()),
             from: getAddressFromAccount(parsed.transactionId.accountId.toString()),
         };
