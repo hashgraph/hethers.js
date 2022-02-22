789799bf207969444e4fdf36fe42f2c5ab7351df
Ivo Yankov <ivo@devlabs.bg>
2022-02-22 10:35:33 +0200

    chore: export additional functions in hethers.utils
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- packages/hethers/src.ts/utils.ts
+++ packages/hethers/src.ts/utils.ts
@@ -1,7 +1,7 @@
 "use strict";
 
 import { AbiCoder, checkResultErrors, ConstructorFragment, defaultAbiCoder, ErrorFragment, EventFragment, FormatTypes, Fragment, FunctionFragment, Indexed, Interface, LogDescription, ParamType, Result, TransactionDescription }from "@ethersproject/abi";
-import { getAddress, getChecksumAddress, getCreate2Address, getIcapAddress, isAddress, getAccountFromAddress, getAddressFromAccount, parseAccount, Account, AccountLike } from "@hethers/address";
+import { getAddress, getChecksumAddress, getCreate2Address, getIcapAddress, isAddress, getAccountFromAddress, getAddressFromAccount, parseAccount, Account, AccountLike, asAccountString, getAccountFromTransactionId } from "@hethers/address";
 import * as base64 from "@ethersproject/base64";
 import { Base58 as base58 } from "@ethersproject/basex";
 import { arrayify, concat, hexConcat, hexDataSlice, hexDataLength, hexlify, hexStripZeros, hexValue, hexZeroPad, isBytes, isBytesLike, isHexString, joinSignature, zeroPad, splitSignature, stripZeros } from "@ethersproject/bytes";
@@ -211,6 +211,8 @@ export {
     getAddressFromAccount,
     getAccountFromAddress,
     parseAccount,
+    asAccountString,
+    getAccountFromTransactionId,
 
     ////////////////////////
     // Hedera Types
